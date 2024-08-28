2113f195447f7a10ee091033a77f9be61221b0f8
Julianscotland <davidscotland2017@gmail.com>
2024-08-28 17:38:20 +0000

    new changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col items-center justify-center pb-8 pt-16 text-white md:flex-grow">
-      <h2> 2024 DSTI Sierra Leone .All right reserver</h2>
-      <h2>Privacy</h2>
-      <h2>Policy</h2>
+    <footer className="text-gray-400">
+      <div className="px-4 sm:px-12 md:flex md:items-center md:justify-between">
+        <h1 className="md:mb:-0 mb-6 lg:leading-normal">
+          <a href="www.dsti.gov.sl">Abous Us</a>
+        </h1>
+        <h1 className="mb-4 mr-1 w-full py-2.5 sm:mr-5 sm:w-72 lg:mb-0">
+          2024 DSTI.All right reserve
+        </h1>
+        <h1 className="w-full px-5 py-2.5 md:w-auto">Privacy Policy</h1>
+      </div>
     </footer>
   );
 };

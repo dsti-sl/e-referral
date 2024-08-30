07c6a206c9153b90ac75b1915c72ce0f205be0b9
Julianscotland <davidscotland2017@gmail.com>
2024-08-30 12:08:49 +0000

    new changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-
+import { CopyrightIcon } from 'lucide-react';
 const Footer = () => {
   return (
-    <footer className="text-gray-400">
+    <footer className="font-extralight text-gray-700">
       <div className="px-4 sm:px-12 md:flex md:items-center md:justify-between">
-        <h1 className="md:mb:-0 lg:leading-normal">
-          <a href="www.dsti.gov.sl">Abous Us</a>
+        <h1 className="md:mb:-0 px-5 py-2.5 lg:leading-normal">
+          <a
+            href="https://www.dsti.gov.sl/about-us/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Abous Us
+          </a>
         </h1>
-        <h1 className="mb-4 mr-1 w-full py-2.5 sm:mr-5 sm:w-72 lg:mb-0">
-          2024 DSTI.All right reserve
-        </h1>
+        <CopyrightIcon className="-mr-72 size-4 px-0 font-thin text-gray-700" />
+        <span className="-ml-72 px-0">2024DSTI.All right reserve</span>
         <h1 className="w-full px-5 py-2.5 md:w-auto">Privacy Policy</h1>
       </div>
     </footer>

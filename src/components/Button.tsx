9806cf5e493f08f53b54c2ac02e25b6f104669db
ShekuMunu <munusheku@gmail.com>
2025-05-09 04:34:12 +0000

    extend button attributes with HTMLButtonElement

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,26 +1,28 @@
 // Reusable next.js built in button component
 
-import { ReactNode, forwardRef } from 'react';
+import { ButtonHTMLAttributes, ReactNode, forwardRef } from 'react';
 
-interface ButtonProps {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
-  onClick: () => void;
   className?: string;
 }
 
-const Button = forwardRef<HTMLButtonElement, ButtonProps>(({ children, onClick, className }, ref) => {
-  return (
-    <button
-      ref={ref}
-      className={`rounded bg-erefer-rose px-4 py-2 text-black hover:bg-white ${className}`}
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  );
-});
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, onClick, className = '', type = 'button', ...rest }, ref) => {
+    return (
+      <button
+        ref={ref}
+        type={type}
+        className={`rounded bg-erefer-rose px-4 py-2 text-black hover:bg-white ${className}`}
+        onClick={onClick}
+        {...rest}
+      >
+        {children}
+      </button>
+    );
+  },
+);
 
 Button.displayName = 'Button';
-
 
 export default Button;

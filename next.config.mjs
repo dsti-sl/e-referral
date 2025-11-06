48f211f246dfbc2da6af7fc96d7cb622ee80f0a8
Iyeba Kallon <iyebalphakallon@gmail.com>
2025-11-06 03:32:14 +0000

    Add: basePath /eref-web prefixing for e-referral web

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
   env: {
     BASE_URL: process.env.BASE_URL,
   },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '/eref-web',
+  assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || '/eref-web/',
 };
 
 export default nextConfig;
